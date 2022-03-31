1e85bb15c0cf1379899f0154b49678ce98118507
Tom Rizzi <t.rizzi@icloud.com>
2022-03-31 20:23:43 +1300

    Changed user agent

--- pricescraper/scraper/settings.py
+++ pricescraper/scraper/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'postman'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
