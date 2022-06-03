6beec2283bd7be6479f995c245ad80ddad932230
Tom Rizzi <t.rizzi@icloud.com>
2022-06-04 10:40:57 +1200

    Updated to use urljoin operation when constructing login path

--- scraper-legacy/pisspricer-scraper/api.py
+++ scraper-legacy/pisspricer-scraper/api.py
@@ -2,6 +2,7 @@ import os
 import requests
 from os.path import join, dirname
 from dotenv import load_dotenv
+from urllib.parse import urljoin
 
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
@@ -10,7 +11,7 @@ url = os.getenv('pisspricer.url')
 email = os.environ.get('pisspricer.email')
 password = os.environ.get('pisspricer.password')
 
-res = requests.post(url + '/users/login', json={"email": email, "password": password})
+res = requests.post(urljoin(url, '/users/login'), json={"email": email, "password": password})
 
 if not res.ok:
     raise Exception(f"Login to API failed ('{email}', '{password}', '{url}')")
